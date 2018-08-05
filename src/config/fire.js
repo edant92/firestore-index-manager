8af5c0c0d3875a3c7036bedabcd7dc7046c60269
Edward <edward@jobcatcher.io>
2018-08-05 17:24:29 +0100

    Fixed test error with projectId not being a string

--- src/config/fire.js
+++ src/config/fire.js
@@ -7,7 +7,7 @@ let firebase_config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
 };
@@ -36,8 +36,6 @@ fire.firestore().enablePersistence()
 export default fire;
 
 export const firebaseAuth = fire.auth();
-
-export const facebookProvider = new firebase.auth.FacebookAuthProvider();
 
 export const googleProvider = new firebase.auth.GoogleAuthProvider();
 
